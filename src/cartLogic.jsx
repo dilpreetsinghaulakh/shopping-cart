dc525cfd2331b0b5a222391dc583aa396a8799d3
Dilpreet Singh <dilpreetsinghaulakh@icloud.com>
2024-05-17 21:20:51 +0530

    Refactor cart logic

--- src/cartLogic.jsx
+++ src/cartLogic.jsx
@@ -1,26 +1,9 @@
-import { useEffect, useState } from "react";
-
 export function createCart() {
   localStorage.setItem("cart", JSON.stringify({}));
 }
 
-export function getCartCount() {
-  const cart = JSON.parse(localStorage.getItem("cart"));
-  const [cartCount, setCartCount] = useState(0);
-  let count = 0;
-
-  for (const key in cart) {
-    count += cart[key];
-  }
-  useEffect(() => {
-    setCartCount(count);
-  }, [cart]);
-
-  return cartCount;
-}
-
 export function addToCart(productId, count) {
-  const cart = JSON.parse(localStorage.getItem("cart"));
+  const cart = getCart();
   if (cart[productId]) {
     cart[productId] += count;
   } else {
@@ -28,21 +11,18 @@ export function addToCart(productId, count) {
   }
 
   localStorage.setItem("cart", JSON.stringify(cart));
-  getCartCount();
 }
 
 export function updateCart(productId, count) {
   const cart = JSON.parse(localStorage.getItem("cart"));
   cart[productId] = count;
   localStorage.setItem("cart", JSON.stringify(cart));
-  getCartCount();
 }
 
 export function removeFromCart(productId) {
   const cart = JSON.parse(localStorage.getItem("cart"));
   delete cart[productId];
   localStorage.setItem("cart", JSON.stringify(cart));
-  getCartCount();
 }
 
 export default function getCart() {
